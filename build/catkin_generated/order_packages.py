644223772d9026f7f1123900d2e05e68ab6590ee
zhongdh7 <zhongdh7@mail2.sysu.edu.cn>
2026-07-30 15:04:19 +0800

    解决了多个TF的问题目前可以用于explorer-v2的teb导航的功能

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
 source_root_dir = '/home/loser/SimEnv/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
+whitelisted_packages = 'unitree_guide;unitree_legged_msgs'.split(';') if 'unitree_guide;unitree_legged_msgs' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/loser/SimEnv/devel;/opt/ros/noetic'.split(';') if '/home/loser/SimEnv/devel;/opt/ros/noetic' != '' else []
--- build/unitree_guide/unitree_ros_to_real/unitree_legged_msgs/catkin_generated/installspace/unitree_legged_msgsConfig.cmake
+++ build/unitree_guide/unitree_ros_to_real/unitree_legged_msgs/catkin_generated/installspace/unitree_legged_msgsConfig.cmake
@@ -156,7 +156,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/loser/SimEnv/install/lib;/opt/ros/noetic/lib)
+    foreach(path /home/loser/SimEnv/install/lib;/home/loser/SimEnv/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
